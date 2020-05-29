1c92531d151ad958268762b7f6395e73bd66432e
DenisGameza <62766333+DenisGameza@users.noreply.github.com>
2020-05-29 21:49:06 +0300

    Add files via upload

---
+++ lab08/MyClass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MyClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher publ = new Publisher("test", "ta@mail.ru", 1000, new DateTime(2014, 10, 16));
+            Book b2 = new Book("Толстой Л.Н.", "Война и мир", publ, 1246, 2016, 18, true);
+            b2.TakeItem();
+            Audit.RunAudit();
+            Magazine mag1 = new Magazine("Обо всём", 4, "Правда", 2016, 12, true);
+            mag1.TakeItem();
+            mag1.Subs();
+            Audit.StopAudit();
+            Magazine mag2 = new Magazine("О разуме", 6, "Горе", 2015, 8, true);
+            Console.WriteLine("\n Тестирование полиморфизма");
+            b2.ReturnSrok();
+            Item it;
+            it = b2;
+            it.TakeItem();
+            it.Return();
+            it = mag1;
+            it.TakeItem();
+            it.Return();
+            List<Item> list = new List<Item>();
+            list.AddRange(new Item[] { b2, mag1 });
+            list.Sort();
+            Console.WriteLine("\nСортировка по инвентарному номеру");
+            foreach (Item x in list){
+                x.Print(); }
+
+        }
+    }
+}
---
+++ lab08/Progressions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Progressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithmeticProgression arith = new ArithmeticProgression(1, 2);
+            int k = 5;
+            Console.WriteLine("{0} член арифметической прогрессии = {1}", k.ToString(), arith.GetElement(k).ToString());
+            GeometricProgression geom = new GeometricProgression(1, 2);
+            k = 5;
+            Console.WriteLine("{0} член геометрической прогрессии = {1}", k.ToString(), geom.GetElement(k).ToString());
+        }
+    }
+}
