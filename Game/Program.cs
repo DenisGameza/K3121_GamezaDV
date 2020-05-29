0ba03b0e5297f7dc308710e2e3e08d2db16880e6
DenisGameza <62766333+DenisGameza@users.noreply.github.com>
2020-05-29 23:20:58 +0300

    Add files via upload

---
+++ Game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            Game game = new Game(player);
+            game.start();
+            game.choice();
+        }
+    }
+}
